4c40f531e34d29b1de3f9551bf86ab50dbb479a8
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-22 14:45:57 +0530

    Added:UC2_Full_Time_Daily_Emp_Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,17 +2,24 @@ public class EmpWageBuilder {
     public static void main(String[] args) {
         // Display welcome Message
         System.out.println("Welcome to Employee Wage Computation Program");
+     
         
-        // Attendance Checker
         int IS_FULL_TIME = 1;
-        int empChecker=((int)(Math.random()*10))%2;
-        if(empChecker==IS_FULL_TIME) {
+        int WAGE_PER_HOUR = 20;
+        int FULL_TIME_HOUR = 8;
+
+        // Attendance Checketr
+        int empChecker = ((int) (Math.random() * 10)) % 2;
+        if (empChecker == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-        }
-        else {
+        } else {
             System.out.println("Employee is not Present");
         }
 
-        
+
+        // Full Time Daily Emp Wage Computation
+        int Full_Time_Daily_Wage = empChecker * FULL_TIME_HOUR * WAGE_PER_HOUR;
+        System.out.println("Today's total Wage of Employee is :" + Full_Time_Daily_Wage);
+
     }
 }
