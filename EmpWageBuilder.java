852334971e5c160fe09c233e543aa0232c20b4b9
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-22 13:19:00 +0530

    Added:Welcome Message

---
+++ EmpWageBuilder.java
@@ -0,0 +1,6 @@
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        // Display welcome Message
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
