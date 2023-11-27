700fa0c47a6874a83da5e7ea82a7513c6e879129
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-28 01:36:28 +0530

    Added: Company Class to compute total wage

---
+++ Company.java
@@ -0,0 +1,66 @@
+public class Company {
+    String name;
+    private int wage;
+    private int  totalWorkingDay;
+    private int totalWorkingHour;
+    public int totalWage = 0;
+
+    // getter function
+    public int wage() {
+        return this.wage;
+    }
+
+    public int totalWorkingDay() {
+        return this.totalWorkingDay;
+    }
+
+    public int totalWorkingHour() {
+        return this.totalWorkingHour;
+    }
+
+    Company( String name, int wage, int totalWorkingDay,int totalWorkingHour) {
+        this.name = name;
+        this.wage = wage;
+        this.totalWorkingDay=totalWorkingDay;
+        this.totalWorkingHour = totalWorkingHour;
+        this.totalWageComputation(totalWorkingDay, totalWorkingHour);
+    }
+
+   
+
+   
+
+    
+    // totalWageComputation
+
+    private  void totalWageComputation(int totalWorkingDay, int totalWorkingHour) {
+        int currentDay = 0;
+        int totalWage = 0;
+        int currentHour = 0;
+        while (currentHour < totalWorkingHour && currentDay < totalWorkingDay) {
+            int isPresent = (int) ((Math.random() * 10) % 2);
+            int workingHour = 0;
+            int TypeofEmp = (int) ((Math.random() * 10) % 2);
+            switch (TypeofEmp) {
+                case 0: // Part Time
+                    workingHour = 4;
+                    break;
+                case 1: // full time
+                    workingHour = 8;
+                    break;
+                default:
+                    workingHour = 0;
+
+            }
+
+            if (isPresent == 1) {
+                currentDay += 1;
+                currentHour += Math.min(workingHour, totalWorkingHour - currentHour);
+            }
+            System.out.println("Day: "+currentDay + " Total working Hour: "+currentHour);
+
+        }
+        this.totalWage = currentHour * this.wage;
+        
+    }
+}
--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,48 +5,8 @@ public class EmpWageBuilder {
             // Display welcome Message
             System.out.println("Welcome to Employee Wage Computation Program");
  
-           final int Max_Day = 20;
-           final int Max_Hour = 100;
-            
-           Employee E1 = new Employee(20, 8); // Full Time Employee
-           Employee PE1 = new Employee(20, 4); // Part Time Employee
-
-            // Attendance Checker for Full Time Emp
-            int is_Present_Full_Time = E1.AttendanceChecker();
-
-            // Daily FUll Time Emp Wage Computation
-            int Full_Time_Daily_Wage = E1.DailyWageComputation(is_Present_Full_Time);
-            System.out.println("Today's total wage For Full Time Employee is : " + Full_Time_Daily_Wage);
-            
-          // Monthly Full Time Emp Wage Computation
-          int Full_Time_Monthly_Wage = E1.MonthlyWageComputation();
-          System.out.println("Monthly Wage for Full Time Emp is : " + Full_Time_Monthly_Wage);
-
-           // Monthly Full Time Emp Wage Computation using condition
-          System.out.println("Monthly total wage for Full Time Emp using condition is : "
-                  + E1.MonthlyWageComputationCondition(Max_Hour, Max_Day));
-
-                  
-            // Attendance Checker for Part Time Emp;
-            int is_Present_Part_Time = PE1.AttendanceChecker();
-           
-             
-            // Daily Part Time Emp Wage COmputation
-            int Part_Time_Daily_Wage = PE1.DailyWageComputation(is_Present_Part_Time);
-            System.out.println("Today's total wage for Part Time Employee is : " + Part_Time_Daily_Wage);
-           
-
-          
-          // Monthly Part Time Emp Wage Computation
-          int Part_Time_Monthly_Wage = PE1.MonthlyWageComputation();
-          System.out.println("Monthly Wage for Part Time Emp is : " + Part_Time_Monthly_Wage);
-
-
-
-         
-          // Monthly Part time emp wage computation using condition
-         System.out.println("Monthly total wage for Part Time Emp using condition is : " +
-         PE1.MonthlyWageComputationCondition(Max_Hour, Max_Day));
+            Company c1 = new Company("Reliance", 20, 20, 100);
+            System.out.println("Total Wage for " + c1.name+ " is "+c1.totalWage);
 
         }
     }
